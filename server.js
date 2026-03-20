bb23993ff51d72defebb96922e829371507f0c48
st-trongnguyen <trong.nguyen@supremetech.vn>
2026-03-20 15:28:43 +0700

    First commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const products = require('./data/products.json');
 const gemstones = require('./data/gemstones.json');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
